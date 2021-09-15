56d3f67ec0d8d3969f11b0e2905d7e4e19dde9e4
Maxi25554 <maxin25554@gmail.com>
2021-09-15 21:54:46 +1000

    Cloned another repo

---
+++ from_scratch.py
@@ -0,0 +1,98 @@
+import discord
+import os
+from dotenv import load_dotenv
+from discord.ext import commands
+from discord.utils import get
+from discord import FFmpegPCMAudio
+from discord import TextChannel
+from youtube_dl import YoutubeDL
+import ffmpeg
+
+load_dotenv()
+bot = commands.Bot(command_prefix=',')  # prefix our commands with '.'
+
+players = {}
+
+queue = []
+
+@bot.event
+async def on_ready():
+    print('Bot online')
+
+
+# Join vc command
+@bot.command(aliases = ["j", "connect", "c"])
+async def join(ctx):
+    channel = ctx.message.author.voice.channel
+    voice = get(bot.voice_clients, guild=ctx.guild)
+    if voice and voice.is_connected():
+        await ctx.send("Already connected to " + str(channel))
+    else:
+        voice = await channel.connect()
+
+
+# Play audio
+@bot.command(aliases = ["p", "pl"])
+async def play(ctx, url):
+    YDL_OPTIONS = {'format': 'bestaudio', 'noplaylist': 'True'}
+    FFMPEG_OPTIONS = {
+        'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5', 'options': '-vn'}
+    voice = get(bot.voice_clients, guild=ctx.guild)
+
+    if not voice.is_playing():
+        with YoutubeDL(YDL_OPTIONS) as ydl:
+            info = ydl.extract_info(url, download=False)
+        URL = info['url']
+        print (str(URL))
+        await ctx.send(str(URL))
+        voice.play(FFmpegPCMAudio(URL, **FFMPEG_OPTIONS))
+        voice.is_playing()
+        await ctx.send('Bot is playing')
+
+# Check if the bot is already playing
+    else:
+        await ctx.send("Bot is already playing")
+        return
+
+
+# End pause
+@bot.command(aliases = ["r", "re", "res"])
+async def resume(ctx):
+    voice = get(bot.voice_clients, guild=ctx.guild)
+
+    if not voice.is_playing():
+        voice.resume()
+        await ctx.send('Bot is resuming')
+
+
+# Pause music
+@bot.command()
+async def pause(ctx):
+    voice = get(bot.voice_clients, guild=ctx.guild)
+
+    if voice.is_playing():
+        voice.pause()
+        await ctx.send('Music paused')
+
+
+# Stop
+@bot.command()
+async def stop(ctx):
+    voice = get(bot.voice_clients, guild=ctx.guild)
+
+    if voice.is_playing():
+        voice.stop()
+        await ctx.send('Stopping...')
+
+
+# Disconnect from vc
+@bot.command(aliases = ["d", "dis", "disc", "dc", "fuckoff", "leave"])
+async def disconnect(ctx):
+    voice = get(bot.voice_clients, guild=ctx.guild)
+    await voice.disconnect()
+
+
+
+Perms = "137488617024"
+Invite = "https://discord.com/api/oauth2/authorize?client_id=887511460324970496&permissions=137488617024&scope=bot"
+bot.run(os.getenv('TOKEN'))
